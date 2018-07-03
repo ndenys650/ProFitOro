3fca804dcd14fc425aad9cd6a702a5a0ddd852f3
Nate Denys <nate_denys@yahoo.com>
2018-07-02 22:37:58 -0700

    Rest of Nuxt upload
    Connection to Firebase complete, finish node_module upload

---
+++ test/utils.spec.js
@@ -0,0 +1,26 @@
+import { leftPad, numberOfSecondsFromNow, beep } from '~/utils/utils'
+
+describe('utils', () => {
+  describe('leftPad', () => {
+    it('should return the string itself if its length is more than 1', () => {
+      expect(leftPad('01')).toEqual('01')
+    })
+    it('should add a 0 from the left if the entry string is of the length of 1', () => {
+      expect(leftPad('0')).toEqual('00')
+    })
+  })
+})
+
+describe('numberOfSecondsFromNow', () => {
+  it('should return the exact number of seconds from now', () => {
+    Date.now = jest.fn(() => 2000)
+    expect(numberOfSecondsFromNow(1000)).toEqual(1)
+  })
+})
+
+describe('beep', () => {
+  it('should call the Audio.play function', ()
+  => {
+    
+  })
+})
